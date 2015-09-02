8d541d6afad886de92b074fade0384927c6d47ed
donghao <donghao@centos101.(none)>
2015-09-02 11:11:35 +0800

    change the run module to carry a argvment

--- run.py
+++ run.py
@@ -1,11 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'Main Module of the program'
+
+__author__ = 'donghao@wizardquant.com'
+
 import read_and_split as rs
 import info_process as ipr
 import write_file as wf
 from time import clock
-start=clock()
-infos=rs.read_log('LogReader.log')
-finish=clock()
-print finish-start
+import sys
+
+args=sys.argv
+if len(args) == 1:
+	print 'need log file path'
+elif len(args)==2:
+	start=clock()
+	infos=rs.read_log(args[1])
+	finish=clock()
+	print finish-start
+else:
+	print 'too many arguments'
+
 notmatch_info = []
 start=clock()
 datas=ipr.process_strs(infos,notmatch_info)
@@ -13,6 +28,7 @@ finish=clock()
 print finish-start
 
 print len(datas)
+
 start=clock()
 wf.write_err('notmatch_log.dat',notmatch_info)
 finish=clock()
